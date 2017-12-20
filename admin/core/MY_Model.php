0fba5ab9514844d1a426880575f0e532eb87b199
skiy <dev@skiy.net>
2017-12-20 17:54:26 +0800

    update my_model

--- admin/core/MY_Model.php
+++ admin/core/MY_Model.php
@@ -1,18 +1,34 @@
 <?php
 /**
  *
- * MY_Model.php
- * @author  : Skiychan <dev@skiy.net>
- * @link    : http://www.zzzzy.com
- * @created : 2016/06/24
- * @modified:
- * @version : 0.0.1
+ * File:   MY_Model.php
+ * Author: Skiychan <dev@skiy.net>
+ * Created: 2017-12-13
  */
 
 class MY_Model extends CI_Model {
 
     public function __construct() {
         parent::__construct();
+        $this->load->helper('DB_Table');
+    }
+
+    /**
+     * 设置查询数量 或 偏移量
+     * @param $limit 数量 或 偏移量
+     * @return mixed
+     */
+    protected function make_limit($limit) {
+        (is_string($limit) or is_numeric($limit)) && $limit = explode(",", $limit);
+        if (is_array($limit)) {
+            count($limit) == 1 && $limit[] = 0;
+        }
+
+        if (empty($limit) || ! is_array($limit)) {
+            $limit = FALSE;
+        }
+
+        return $limit;
     }
 
     /**
@@ -31,5 +47,12 @@ class MY_Model extends CI_Model {
         }
         $fields = implode(",", $fields_arr);  //最终组合成 f.id,f.name,f.email形式
         return $fields;
+    }
+
+    /**
+     * 写入数据库操作日志
+     */
+    protected function log() {
+        log_message('info', $this->db->last_query());
     }
 }
